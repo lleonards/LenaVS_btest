db501409d28555f4ff6d525501c15b4542e40cda
lleonards <lleonards.777@gmail.com>
2026-08-17 23:25:47 -0300

    Delete LenaVS_btest directory

--- LenaVS_btest/src/routes/sync.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { synchronizeLyricsBlocks } from '../controllers/syncController.js';
-import { authenticateToken } from '../middleware/auth.js';
-import { requireActiveAccess } from '../middleware/requireActiveAccess.js';
-const router = express.Router();
-router.post('/', authenticateToken, requireActiveAccess, synchronizeLyricsBlocks);
-export default router;
