1e678148884108cdaebf3b807c1a3b93a95023c5
lleonards <lleonards.777@gmail.com>
2026-08-22 22:22:01 -0300

    Delete LenaVS_btest directory

--- LenaVS_btest/src/routes/vocalSync.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { authenticateToken } from '../middleware/auth.js';
-import { requireActiveAccess } from '../middleware/requireActiveAccess.js';
-import { synchronizeVocalBlocks } from '../controllers/vocalSyncController.js';
-const router = express.Router();
-router.post('/', authenticateToken, requireActiveAccess, synchronizeVocalBlocks);
-export default router;
